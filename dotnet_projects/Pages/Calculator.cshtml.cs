a7c79739f2d893e61076ea65ea29769a02aab5bb
jon glass <jonglass@jons-MacBook-Air.local>
2020-11-20 23:10:34 -0600

    addadditional calculator feature and clear button

--- dotnet_projects/Pages/Calculator.cshtml.cs
+++ dotnet_projects/Pages/Calculator.cshtml.cs
@@ -9,8 +9,55 @@ namespace dotnet_projects.Pages
 {
     public class CalculatorModel : PageModel
     {
+        public float Val1 { get; set; }
+        public float Val2 { get; set; }
+        public string Operator { get; set; }
+        public string Result { get; set; }
+
         public void OnGet()
         {
+            Console.WriteLine("Hello from the Calculator Model");
         }
+
+        public ActionResult OnPostCalculate()
+        {
+            if(HttpContext.Request.Form["Val1"] == "")
+            {
+                Val1 = float.Parse(Result);
+            }
+            else
+            {
+                Val1 = float.Parse(HttpContext.Request.Form["Val1"]);
+                Val2 = float.Parse(HttpContext.Request.Form["Val2"]);
+                Operator = HttpContext.Request.Form["Operator"];
+            }
+
+            switch (Operator)
+            {
+                case ("+"):
+                    Result = (Val1 + Val2).ToString();
+                    break;
+                case ("x"):
+                    Result = (Val1 * Val2).ToString();
+                    break;
+                case ("/"):
+                    Result = (Val1 / Val2).ToString();
+                    break;
+                case ("-"):
+                    Result = (Val1 - Val2).ToString();
+                    break;
+            }
+
+            return Page();
+        }
+
+        public ActionResult OnPostClear()
+        {
+            Val1 = 0;
+            Val2 = 0;
+            Operator = "";
+            return Page();
+        }
+
     }
 }
--- dotnet_projects/wwwroot/js/site.js
+++ dotnet_projects/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+//Calculator
+
+var val1 = document.getElementById('val1');
+var val2 = document.getElementById('val2');
+var operator = document.getElementById('operator');
+
+function setValue(val) {
+    operator.value.length > 0 ? val2.value += val : val1.value += val;
+}
+
+function setOperator(op) {
+    operator.value = op;
+}
+
+function positiveNegative() {
+    operator.value.length > 0 ? val2.value = "-" + Val2.value : Val1.value = "-" + Val1.value;
+
+}
